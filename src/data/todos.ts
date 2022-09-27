2244d5e4e8296042473ecb2299347e5621bb8b28
bioxcode <ben96hayward@gmail.com>
2022-09-27 18:13:54 +0100

    replace todos data with test success criteria

--- src/data/todos.ts
+++ src/data/todos.ts
@@ -1,11 +1,16 @@
 import { Todo } from '../types/Todo';
 
 const todos: Todo[] = [
-    { text: "Buy some Apples", isCompleted: true },
-    { text: "Buy some Pears", isCompleted: false },
-    { text: "Buy some Bananas", isCompleted: false },
-    { text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer cursus mi quis dui molestie semper. Vivamus nisl turpis, semper ac sem quis, sollicitudin sodales neque. Cras sed luctus augue.", isCompleted: false },
+    { text: "Use React context to provide data and methods", isCompleted: true },
+    { text: "Use React hooks and create a custom hook that manages the todo data.", isCompleted: false },
+    { text: "Add new todos", isCompleted: false },
+    { text: "Toggle completeness of todos", isCompleted: false },
+    { text: "Delete individual todos", isCompleted: false },
+    { text: "Clear all todos with a yes/no confirmation", isCompleted: false },
+    { text: "Fix application code to make existing failing test pass", isCompleted: false },
+    { text: "Complete the incomplete test case", isCompleted: false },
 ];
+
 
 export default todos;
 
